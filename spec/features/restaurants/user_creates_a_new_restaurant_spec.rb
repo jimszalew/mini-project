d93242384a3773697c59a069435767bc89d73cc9
Jim Szalewski <jim.szalewski@gmail.com>
2017-07-19 14:28:52 -0600

    add more columns to restaurant

--- spec/features/restaurants/user_creates_a_new_restaurant_spec.rb
+++ spec/features/restaurants/user_creates_a_new_restaurant_spec.rb
@@ -5,10 +5,13 @@ describe "User creates a new restaurant" do
     visit new_restaurant_path
 
     fill_in "restaurant[name]", with: "Fruition"
+    fill_in "restaurant[description]", with: "An Americana eatery focusing on locally sourced sustainable fresh ingredients."
+    fill_in "restaurant[address]", with: "123 Some St. Denver CO"
+    fill_in "restaurant[phone]", with: "720-555-5555"
     click_button "Create"
+
 
     expect(current_path).to eq("/restaurants/#{Restaurant.last.id}")
     expect(page).to have_content("Fruition")
-    expect(Restaurant.count).to eq(1)
   end
 end
