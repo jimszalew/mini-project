50fd3d6a14c0b7c10eef4cd02db035350b5c166a
Jim Szalewski <jim.szalewski@gmail.com>
2017-07-19 15:19:27 -0600

    restaurant factory sequenced out

--- spec/factories/restaurants.rb
+++ spec/factories/restaurants.rb
@@ -1,8 +1,16 @@
 FactoryGirl.define do
   factory :restaurant do
-    name "Fruition"
-    description "An Americana eatery focusing on locally sourced sustainable fresh ingredients."
-    address "123 Some St. Denver CO"
-    phone "720-555-5555"
+    sequence :name do |i|
+      "#{i}Fruition"
+    end
+    sequence :description do |i|
+      "#{i}An Americana eatery focusing on locally sourced sustainable fresh ingredients."
+    end
+    sequence :address do |i|
+      "#{i}123 Some St. Denver CO"
+    end
+    sequence :phone do |i|
+      "#{i}720-555-5555"
+    end
   end
 end
